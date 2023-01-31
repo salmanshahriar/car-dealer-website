6f89ddd25edb7ae1259b95629d78edaa3260d75f
salmanshahriar <salmanshahriar16@gmail.com>
2023-02-01 02:24:05 +0600

    hambuger menu added, tab view added

---
+++ app/main.js
@@ -0,0 +1,7 @@
+// burgerMenu
+var burgerMenu = document.getElementById('burger-menu');
+var overlay = document.getElementById('menu');
+burgerMenu.addEventListener('click',function(){
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
